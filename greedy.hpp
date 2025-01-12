400517dd5c78a8979122f973aefd992df547b782
ritheus <richardtheus7@gmail.com>
2025-01-12 23:47:54 +0100

    Replace custom implementation with external priority queue

--- greedy.hpp
+++ greedy.hpp
@@ -1,16 +1,18 @@
 #pragma once
 
 #include "aliases.hpp"
-#include "pq.hpp"
+#include "updatable_priority_queue.hpp"
 #include <vector>
 #include <unordered_set>
 #include <atomic>
 
 
-extern std::atomic<bool> sigterm_received;
+namespace Greedy {
+	extern std::atomic<bool> sigterm_received;
 
-std::vector<Node> calculateSolution(NumNodes&, NumEdges&, std::vector<std::vector<Node>>&);
-std::tuple<Node, uint32_t> findMajorityNode(const PriorityQueue<Node, uint32_t>&);
-void updateCount(PriorityQueue<Node, uint32_t>&, const std::vector<std::vector<Node>>&, const std::vector<std::vector<Node>>&);
-std::vector<std::vector<Node>> shrinkSolutionIfApplicable(const NumNodes, std::vector<Node>&, std::vector<std::vector<Node>>&, uint32_t, std::vector<std::vector<Node>>&);
-std::vector<std::vector<Node>> removeEdgesContainingNode(Node, std::vector<std::vector<Node>>&, std::vector<std::vector<Node>>&);
+	std::vector<Node> calculateSolution(NumNodes&, NumEdges&, std::vector<std::vector<Node>>&);
+	std::tuple<Node, uint32_t> findMajorityNode(const pq::updatable_priority_queue<Node, uint32_t>&);
+	void setCount(pq::updatable_priority_queue<Node, uint32_t>&, const std::vector<std::vector<Node>>&);
+	void updateCount(pq::updatable_priority_queue<Node, uint32_t>&, const std::vector<std::vector<Node>>&);
+	std::vector<std::vector<Node>> removeEdgesContainingNode(Node, std::vector<std::vector<Node>>&, std::vector<std::vector<Node>>&);
+}
--- main.cpp
+++ main.cpp
@@ -1,28 +1,49 @@
 #include "utils.hpp"
+#include "adaptiveGreedy.hpp"
 #include "greedy.hpp"
 #include <iostream>
 #include <csignal>
 
 void signalHandler(int signal) {
     if (signal == SIGTERM) {
-        sigterm_received = true;
+        Greedy::sigterm_received = true;
     }
 }
 
 int main(int argc, char *argv[]) {
 #if _DEBUG
-    argc = 2;
-    const char* fakeArgv[] = { argv[0], "bremen_subgraph_20.hgr" };
+    argc = 3;
+    const char* fakeArgv[] = { argv[0], "greedy", "bremen_subgraph_20.hgr"};
     argv = const_cast<char**>(fakeArgv);
 #endif
 
+    bool useGreedy = false;
+    bool useAdaptiveGreedy = false;
+    if (strcmp(argv[1], "greedy") == 0) {
+        useGreedy = true;
+    }
+    if (strcmp(argv[1], "adaptiveGreedy") == 0) {
+        useAdaptiveGreedy = true;
+    }
+
     // Modell einlesen
-    //auto [n, m, setSystem] = parseInputFile(argv[1]);
+#if _DEBUG
+    auto [n, m, setSystem] = parseInputFile(argv[2]);
+#endif
+#ifndef _DEBUG
     auto [n, m, setSystem] = parseStdIn();
+#endif
+
     std::signal(SIGTERM, signalHandler);
 
     // Berechnung
-    std::vector<Node> solution = calculateSolution(n, m, setSystem);
+    std::vector<Node> solution;
+    if (useGreedy) {
+        solution = Greedy::calculateSolution(n, m, setSystem);
+    }
+    else if (useAdaptiveGreedy) {
+        solution = AdaptiveGreedy::calculateSolution(n, m, setSystem);
+    }
 
     // Lösung in stdout ausgeben
     writeToStdOut(solution);
